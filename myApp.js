201546d9f0f9255b2ce0421c65eace7d0d48f413
ankita-sharma10 <67052288+ankita-sharma10@users.noreply.github.com>
2021-08-17 17:29:12 +0530

    test code

--- myApp.js
+++ myApp.js
@@ -4,7 +4,27 @@ var app = express();
 console.log("Hello World")
 
 app.get('/', function (req, res) {
-    res.send('Hello Express');
+    const request = require('request');
+    let options = {
+        'method': 'POST',
+        'url': 'https://preview.compport.com/api/v1/core_reports/current_turnover',
+        'headers': {
+            'auth': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkZXQiOiJBZG1pbkB2YXB0LmNvbSIsInVpIjoiMSIsInJvbGUiOiIxIiwidW5pdCI6IjExIn0.g8K2almHd4GWi0ndkU8HGpOQ7B2MHBQLXPZRrTYvd-A',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "start_date": "19/08/2020",
+            "end_date": "12/08/2021"
+        })
+
+    };
+    request(options, (error, response) => {
+        if(error){
+            res.send(error)
+        }else{
+            res.send(response)
+        }
+    });
 })
 
 
