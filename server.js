e56e780b7a7905c7ea7a7780343bedf8910ee613
Ankita Sharma <sharma003ankita@gmail.com>
2022-04-06 17:34:35 +0530

    Update server.js
    Test code for compport

--- server.js
+++ server.js
@@ -10,6 +10,7 @@
  
  if (!process.env.DISABLE_XORIGIN) {
    app.use(function(req, res, next) {
+    
      var allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
      var origin = req.headers.origin || '*';
      if(!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1){
@@ -20,6 +21,10 @@
      next();
    });
  }
+
+app.get("/test_route_compport" , function(req , res){
+ res.send({ip : req.ip})
+})
  
  var port = process.env.PORT || 3000;
  bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function(){
